229df1afc32abfc4125aef4fabe4f8cf20712a69
Piotr Kowalski <piet.kowalski@gmail.com>
2019-01-19 15:02:16 +0100

    Adjust error handler - take validation error into account

--- routes/errorHandler.js
+++ routes/errorHandler.js
@@ -2,8 +2,20 @@ module.exports = (err, req, res, next) => {
     if (!err) {
         return next();
     }
-    res.json({
-        errorMessage: err.message || res.errorMessage || "Don't panic!",
+    if (err.name === 'ValidationError') {
+        if (!err.statusCode) {
+            res.statusCode = 422;
+        }
+    }
+
+    const payload = {
+        errorMessage: err._message || err.message || res.errorMessage || "Don't panic!",
         errorCode: err.statusCode || res.statusCode || 500,
-    });
+    };
+
+    if (process.env.NODE_ENV === 'development') {
+        payload.error = err;
+    }
+
+    res.json(payload);
 };
