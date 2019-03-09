4425d2d8af571e5095bd9453e1bd5ccf8cc86dec
Piotr Kowalski <piet.kowalski@gmail.com>
2019-03-09 23:47:07 +0100

    Update check params middleware

--- middleware/orderCreateParamsCheck.js
+++ middleware/orderCreateParamsCheck.js
@@ -3,53 +3,53 @@ module.exports = (req, res, next) => {
 
     if (typeof body.notifyUrl !== 'string') {
         res.status(401);
-        return next(Error('Invalid notify url'));
+        return next(new Error('Invalid notifyUrl parameter'));
     }
     if (typeof body.merchantPosId !== 'string') {
         res.status(401);
-        return next(Error('Invalid point of sale'));
+        return next(new Error('Invalid merchantPosId parameter'));
     }
     if (typeof body.description !== 'string') {
         res.status(401);
-        return next(Error('Invalid description'));
+        return next(new Error('Invalid description parameter'));
     }
     if (typeof body.currencyCode !== 'string') {
         res.status(401);
-        return next(Error('Invalid currency code'));
+        return next(new Error('Invalid currencyCode parameter'));
     }
     if (typeof body.totalAmount !== 'string') {
         res.status(401);
-        return next(Error('Invalid total amount'));
+        return next(new Error('Invalid totalAmount parameter'));
     }
     if (typeof body.notifyUrl !== 'string') {
         res.status(401);
-        return next(Error('Invalid notify url'));
+        return next(new Error('Invalid notifyUrl parameter'));
     }
     if (typeof body.continueUrl !== 'string') {
         res.status(401);
-        return next(Error('Invalid continue url'));
+        return next(new Error('Invalid continueUrl parameter'));
     }
 
     // TODO: Validate props of objects below
     const buyer = body.buyer;
     if (typeof buyer !== 'object') {
         res.status(401);
-        return next(Error('Invalid buyer'));
+        return next(new Error('Invalid buyer parameter'));
     }
     const settings = body.settings;
     if (typeof settings !== 'object') {
         res.status(401);
-        return next(Error('Invalid settings'));
+        return next(new Error('Invalid settings parameter'));
     }
     const products = body.products;
     if (typeof products !== 'object') {
         res.status(401);
-        return next(Error('Invalid products'));
+        return next(new Error('Invalid products parameter'));
     }
     const payMethods = body.payMethods;
     if (typeof payMethods !== 'object') {
         res.status(401);
-        return next(Error('Invalid pay methods'));
+        return next(new Error('Invalid payMethods parameter'));
     }
 
     next();
