caf97da0b543cedd4d23dfcfcfc854b8a0147ab0
Piotr Kowalski <piet.kowalski@gmail.com>
2019-03-16 14:24:04 +0100

    Refactor mailTransporter module - get rid of dev/prod check

--- utils/mailTransporter.js
+++ utils/mailTransporter.js
@@ -1,25 +1,11 @@
 const nodemailer = require('nodemailer');
 
-let mailConfig;
-
-if (process.env.NODE_ENV === 'production' ) {
-    mailConfig = {
-        host: 'smtp.sendgrid.net',
-        port: 587,
-        auth: {
-            user: 'real.user',
-            pass: 'verysecret'
-        }
-    };
-} else {
-    mailConfig = {
-        host: process.env.EMAIL_ACCOUNT_SMTP_HOST,
-        port: process.env.EMAIL_ACCOUNT_SMTP_PORT,
-        secure: !!process.env.EMAIL_ACCOUNT_SMTP_SECURE,
-        auth: {
-            user: process.env.EMAIL_ACCOUNT_USER,
-            pass: process.env.EMAIL_ACCOUNT_PASS,
-        }
-    };
-}
-module.exports = nodemailer.createTransport(mailConfig);
+module.exports = nodemailer.createTransport({
+    host: process.env.EMAIL_ACCOUNT_SMTP_HOST,
+    port: process.env.EMAIL_ACCOUNT_SMTP_PORT,
+    secure: !!process.env.EMAIL_ACCOUNT_SMTP_SECURE,
+    auth: {
+        user: process.env.EMAIL_ACCOUNT_USER,
+        pass: process.env.EMAIL_ACCOUNT_PASS,
+    }
+});
