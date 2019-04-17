8d74935defb3c8cdfb5be394c41200f27d6c6941
Piotr Kowalski <piet.kowalski@gmail.com>
2019-04-17 15:41:05 +0200

    Refactor products check middleware

--- middleware/productsCheck.js
+++ middleware/productsCheck.js
@@ -1,35 +1,37 @@
-const productsCheck = ProductModel => async (req, res, next) => {
-    const {body: {products = {}, productsIds = []}} = req;
-    if (!productsIds.length) {
-        res.status(400);
-        return next(new Error('Empty products ids'));
-    }
-    productsIds.forEach(async productId => {
-        const productData = products[productId];
-        if (!productData) {
+const productsCheck = ProductModel => (req, res, next) => {
+    try {
+        const {body: {products = {}, productsIds = []}} = req;
+        if (!productsIds.length) {
             res.status(400);
-            return next(new Error(`No products[${productId}] data`));
+            return next(new Error('Empty products ids'));
         }
-        try {
-            const {unitPrice, stock, name} = await ProductModel.findById(productId).exec();
-            if (productData.unitPrice / 100 !== parseFloat(unitPrice)) {
+        productsIds.forEach(productId => {
+            const productData = products[productId];
+            if (!productData) {
                 res.status(400);
-                return next(new Error('Wrong product unit price'));
+                return next(new Error(`No products[${productId}] data`));
             }
-            if (parseInt(productData.quantity, 2) > stock) {
-                res.status(400);
-                return next(new Error('Wrong product quantity'));
-            }
-            if (productData.name !== name) {
-                res.status(400);
-                return next(new Error('Wrong product name'));
-            }
-        } catch (err) {
-            return next(err);
-        }
-
-    });
-    return next();
+            ProductModel.findById(productId, function(err, product) {
+                const {unitPrice, stock, name} = product;
+                if (productData.unitPrice / 100 !== parseFloat(unitPrice)) {
+                    res.status(400);
+                    return next(new Error('Wrong product unit price'));
+                }
+                if (parseInt(productData.quantity, 2) > stock) {
+                    res.status(400);
+                    return next(new Error('Wrong product quantity'));
+                }
+                if (productData.name !== name) {
+                    res.status(400);
+                    return next(new Error('Wrong product name'));
+                }
+                next();
+            });
+        });
+    } catch (err) {
+        res.status(400);
+        next(err);
+    }
 };
 
 module.exports = productsCheck;
