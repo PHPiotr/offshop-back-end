a185a5252174203be5fb34a26e4b55e64a27826c
Piotr Kowalski <piet.kowalski@gmail.com>
2019-04-21 14:07:21 +0200

    Decouple dev:serve script from package.json

---
+++ scripts/serve.js
@@ -0,0 +1,10 @@
+'use strict';
+
+process.env.NODE_ENV = 'development';
+
+const dotenv = require('dotenv');
+const childProcess = require('child_process');
+
+dotenv.load();
+
+childProcess.exec(`ssh -o ServerAliveInterval=${process.env.SERVER_ALIVE_INTERVAL} -R ${process.env.DOMAIN_NAME}.serveo.net:80:localhost:${process.env.PORT} serveo.net`);
