17a8c3998473a307661007e1a189818f59e5a3b2
Piotr Kowalski <piet.kowalski@gmail.com>
2019-04-25 13:21:51 +0200

    See what serve script does - add console.log

--- scripts/serve.js
+++ scripts/serve.js
@@ -6,5 +6,5 @@ const dotenv = require('dotenv');
 const childProcess = require('child_process');
 
 dotenv.load();
-
+console.log(`ssh -o ServerAliveInterval=${process.env.SERVER_ALIVE_INTERVAL} -R ${process.env.DOMAIN_NAME}.serveo.net:80:localhost:${process.env.PORT} serveo.net`);
 childProcess.exec(`ssh -o ServerAliveInterval=${process.env.SERVER_ALIVE_INTERVAL} -R ${process.env.DOMAIN_NAME}.serveo.net:80:localhost:${process.env.PORT} serveo.net`);
