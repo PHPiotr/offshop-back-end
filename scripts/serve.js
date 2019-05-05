41e366a0575fd46d72a7252e81d99286be4988f2
Piotr Kowalski <piet.kowalski@gmail.com>
2019-05-06 00:57:33 +0200

    Upgrade * add engine prop to package.json (node 12.1.0, npm 6.9.0) * npm update (dotenv, nodemailer, sharp) * use built-in crypto * fix `dotenv.load` is not a function

--- scripts/serve.js
+++ scripts/serve.js
@@ -5,6 +5,6 @@ process.env.NODE_ENV = 'development';
 const dotenv = require('dotenv');
 const childProcess = require('child_process');
 
-dotenv.load();
+dotenv.config();
 console.log(`ssh -o ServerAliveInterval=${process.env.SERVER_ALIVE_INTERVAL} -R ${process.env.DOMAIN_NAME}.serveo.net:80:localhost:${process.env.PORT} serveo.net`);
 childProcess.exec(`ssh -o ServerAliveInterval=${process.env.SERVER_ALIVE_INTERVAL} -R ${process.env.DOMAIN_NAME}.serveo.net:80:localhost:${process.env.PORT} serveo.net`);
